0bf6e3af11a75013f502e712dfd1e7432398299e
Rafi Khan <zetoslab@gmail.com>
2017-06-12 18:09:19 -0600

    refactored and added status bar

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+
+
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.title = 'Stet pyQt'
+
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.statusBar().showMessage('Initialized Stet')
+        self.show()
+
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.resize(250, 250)
-    w.move(300, 300)
-    w.setWindowTitle('Stet pyQt')
-    w.show()
+    ex = App()
 
     sys.exit(app.exec_())
