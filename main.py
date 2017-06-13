1d781f69ec677e38f908433358046cf45b46b013
Rafi Khan <zetoslab@gmail.com>
2017-06-12 18:01:33 -0600

    create main window

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
 if __name__ == '__main__':
-    print("Started Stet pyQT")
+
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(250, 250)
+    w.move(300, 300)
+    w.setWindowTitle('Stet pyQt')
+    w.show()
+
+    sys.exit(app.exec_())
